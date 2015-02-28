4cdb8aac094d1ca5898fddd13194ecdeaa11c144
RogachevAndrey <angorow@bk.ru>
2015-02-28 21:01:33 +0300

    222

--- app/src/main/java/com/angorow/thing/Thing.java
+++ app/src/main/java/com/angorow/thing/Thing.java
@@ -3,8 +3,26 @@ package com.angorow.thing;
 /**
  * Created by Alyaska on 28.02.2015.
  */
+
+import android.graphics.Rect;
+
 public abstract class Thing {
 
-    protected Thing() {
+    private Rect _bounds;
+
+    protected Thing(Rect bounds) throws Exception {
+
+        if (bounds == null) throw new Exception();
+
+        _bounds = bounds;
     }
+
+    public Rect get_bounds() {
+        return _bounds;
+    }
+
+    public void set_bounds(Rect _bounds) {
+        this._bounds = _bounds;
+    }
+
 }
